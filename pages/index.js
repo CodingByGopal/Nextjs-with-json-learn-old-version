17264853ff6f1c8775730f8c6b696391d666e771
CodingByGopal <gopalji028@gmail.com>
2022-11-24 22:31:51 +0530

    Client side and server side data fetching learned

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,21 @@ export default function Home() {
       <Link href="/posts" passHref>
         <p>Posts</p>
       </Link>
+      <Link href="/news" passHref>
+        <p>News</p>
+      </Link>
+      <Link href="/news/sports" passHref>
+        <p>Sports News</p>
+      </Link>
+      <Link href="/news/politics" passHref>
+        <p>Politics News</p>
+      </Link>
+      <Link href="/dashboard" passHref>
+        <p>Dashboard with fetch api</p>
+      </Link>
+      <Link href="/dashboard-swr" passHref>
+        <p>Dashboard with SWR that is React Hooks for Data Fetching</p>
+      </Link>
     </div>
   );
 }
---
+++ pages/news/[category].js
@@ -0,0 +1,43 @@
+function ArticleListByCategory({ articles, category }) {
+  return (
+    <>
+      <h1>
+        Showing news for category <i>{category}</i>
+      </h1>
+      {articles.map((article) => {
+        return (
+          <div key={article.id}>
+            <h2>
+              {article.id} {article.title}
+            </h2>
+
+            <p>{article.description}</p>
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default ArticleListByCategory;
+
+export async function getServerSideProps(context) {
+  const { params, req, res, query } = context;
+  console.log(query);
+  console.log(req.headers.cookie);
+  res.setHeader("Set-Cookie", ["name=Gopal"]);
+
+  const { category } = params;
+  const response = await fetch(
+    `http://localhost:4000/news?category=${category}`
+  );
+
+  const data = await response.json();
+  return {
+    props: {
+      articles: data,
+      category,
+    },
+  };
+}
